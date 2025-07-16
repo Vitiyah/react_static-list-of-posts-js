556d47ea6c2e86b274af7012358b59ae3ed4e21a
vtyacodes <vitiyah95@gmail.com>
2025-07-16 22:34:13 +0800

    solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,33 @@
-export const PostInfo = () => <>Put the post here</>;
+import commentsFromServers from '../../api/comments.json';
+import { CommentList } from '../CommentList/CommentList';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const PostInfo = ({ post }) => {
+  const filteredComments = commentsFromServers.filter(
+    comments => comments.postId === post.id,
+  );
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <p>
+          {' Posted by  '}
+
+          <UserInfo userId={post.userId} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      <hr />
+
+      {filteredComments.length > 0 ? (
+        <CommentList comments={filteredComments} />
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
+    </div>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,11 @@
-export const UserInfo = () => <>Put the user here</>;
+import usersFromServer from '../../api/users.json';
+
+export const UserInfo = ({ userId }) => {
+  const user = usersFromServer.find(usr => usr.id === userId);
+
+  return (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  );
+};
